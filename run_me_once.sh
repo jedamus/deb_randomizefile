baa80ff79387eb1bbd1bceefc7873ba20264fbe4
Leander Jedamus <ljedamus@web.de>
2019-12-16 11:15:09 +0100

    minor changes

--- run_me_once.sh
+++ run_me_once.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # erzeugt Mittwoch, 20. November 2019 14:15 (C) 2019 von Leander Jedamus
-# modifiziert Montag, 16. Dezember 2019 11:02 von Leander Jedamus
+# modifiziert Montag, 16. Dezember 2019 11:14 von Leander Jedamus
 # modifiziert Montag, 02. Dezember 2019 02:53 von Leander Jedamus
 # modifiziert Montag, 25. November 2019 10:09 von Leander Jedamus
 # modifiziert Donnerstag, 21. November 2019 07:10 von Leander Jedamus
@@ -17,7 +17,7 @@ make -W translate.sh.in translate.sh
 src/randomizefile
 sleep 10
 make distcheck
-rm debian/randomizefile.debhelper.log
+rm -f debian/randomizefile.debhelper.log
 dh binary-arch --parallel
 sudo dh binary-arch --parallel
 sudo dpkg -i ../randomizefile_1.2_amd64.deb
