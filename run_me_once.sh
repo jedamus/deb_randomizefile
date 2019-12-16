e84711520eef002c072758c05ddb58603b5c2b2e
Leander Jedamus <ljedamus@web.de>
2019-12-16 09:34:53 +0100

    added dpkg-command to run_me_once.sh

--- run_me_once.sh
+++ run_me_once.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # erzeugt Mittwoch, 20. November 2019 14:15 (C) 2019 von Leander Jedamus
-# modifiziert Montag, 16. Dezember 2019 09:25 von Leander Jedamus
+# modifiziert Montag, 16. Dezember 2019 09:34 von Leander Jedamus
 # modifiziert Montag, 02. Dezember 2019 02:53 von Leander Jedamus
 # modifiziert Montag, 25. November 2019 10:09 von Leander Jedamus
 # modifiziert Donnerstag, 21. November 2019 07:10 von Leander Jedamus
@@ -19,6 +19,7 @@ sleep 10
 make distcheck
 dh binary-arch --parallel
 sudo dh binary-arch --parallel
+sudo dpkg -i ../randomizefile_1.2_amd64.deb
 
 # vim:ai sw=2
 
