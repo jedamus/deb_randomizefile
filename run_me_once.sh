2e2ceff875e90208292f5ca42ff5702c4b6edace
Leander Jedamus <ljedamus@web.de>
2019-12-16 09:26:20 +0100

    added dh call to run_me_once.sh

--- run_me_once.sh
+++ run_me_once.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 # erzeugt Mittwoch, 20. November 2019 14:15 (C) 2019 von Leander Jedamus
+# modifiziert Montag, 16. Dezember 2019 09:25 von Leander Jedamus
 # modifiziert Montag, 02. Dezember 2019 02:53 von Leander Jedamus
 # modifiziert Montag, 25. November 2019 10:09 von Leander Jedamus
 # modifiziert Donnerstag, 21. November 2019 07:10 von Leander Jedamus
@@ -11,11 +12,13 @@ gettextize -f
 mv po/Makevars.template po/Makevars
 ./configure
 make
-make translate.sh
+make -W translate.sh.in translate.sh
 ./translate.sh
 src/am_randomize_file
 sleep 10
 make distcheck
+dh binary-arch --parallel
+sudo dh binary-arch --parallel
 
 # vim:ai sw=2
 
